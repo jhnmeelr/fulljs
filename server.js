1386d1b8d9f1c414a3bff7d6b637a5da25316fa0
jhnmeelr <jhn.meelr@gmail.com>
2016-11-27 18:18:22 +0200

    Rendering on the Server

---
+++ server.js
@@ -0,0 +1,33 @@
+import config from './config';
+import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+import serverRender from './serverRender';
+
+server.get('/', (req, res) => {
+    serverRender()
+        .then(({ initialMarkup, initialData }) => {
+            res.render('index', {
+                initialMarkup,
+                initialData
+            });
+        })
+        .catch(console.error);
+});
+
+server.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public')
+}));
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, config.host, () => {
+    console.info('Express listening on port', config.port);
+});
---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Header from './Header';
+import ContestPreview from './ContestPreview';
+
+class App extends React.Component {
+    state = {
+        pageHeader: 'Naming Contests',
+        contests: this.props.initialContests
+    }
+    render() {
+        return (
+            <div className="App">
+                <Header message={this.state.pageHeader} />
+                <div>
+                    {
+                        this.state.contests.map(contest => <ContestPreview key={contest.id} {...contest} />)
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
